04c75ce30bc759b98a64f6157235317bc13f852c
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-04-03 14:46:58 +0700

    fix typo

--- examples/timeboard-usrpapi-lbint-01/outputs.tf
+++ examples/timeboard-usrpapi-lbint-01/outputs.tf
@@ -1,3 +1,3 @@
 output "timeboard_elb_classic_usrpapi-lbint-01_title" {
-  value = "${module.timeboard_elb_application_entcurr-lbint-01.title}"
+  value = "${module.timeboard_elb_classic_usrpapi-lbint-01.title}"
 }
