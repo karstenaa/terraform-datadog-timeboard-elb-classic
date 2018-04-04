4fde034408462ed53f58e4456d9e2b97d3e444ea
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-04-04 16:09:06 +0700

    remove datadog parameter

--- examples/timeboard-usrpapi-lbint-01/main.tf
+++ examples/timeboard-usrpapi-lbint-01/main.tf
@@ -1,8 +1,3 @@
-provider "datadog" {
-  api_key = "${var.datadog_api_key}"
-  app_key = "${var.datadog_app_key}"
-}
-
 module "timeboard_elb_classic_usrpapi-lbint-01" {
   source         = "../../"
   product_domain = "${var.product_domain}"
